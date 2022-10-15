9c3e92b8eb9706cb65e364025ea4e5bbae3aeb07
PRODEV <dreamworld@DREAMWORLDs-iMac-Pro.local>
2022-10-15 15:18:53 +0530

    fix version issue for xcode 14

--- Sources/RegularPlayer.swift
+++ Sources/RegularPlayer.swift
@@ -6,6 +6,7 @@
 //
 //
 
+import UIKit
 import Foundation
 import AVFoundation
 import AVKit
@@ -22,74 +23,58 @@ extension AVMediaSelectionOption: TextTrackMetadata {
         public static let TimeUpdateInterval: TimeInterval = 0.1
     }
     
-    // MARK: - Private Properties
+    // MARK: Private Properties
     
     fileprivate var player = AVPlayer()
-
-    private var regularPlayerView: RegularPlayerView
-
-    private var playerLayer: AVPlayerLayer {
-        return self.regularPlayerView.playerLayer
-    }
-
-    private var seekTolerance: CMTime?
-
-    private var seekTarget: CMTime = CMTime.invalid
-    private var isSeekInProgress: Bool = false
     
-    // MARK: - Public API
+    // MARK: Public API
     
     /// Sets an AVAsset on the player.
     ///
     /// - Parameter asset: The AVAsset
     @objc open func set(_ asset: AVAsset) {
-        let playerItem = AVPlayerItem(asset: asset)
-        self.set(playerItem: playerItem)
-    }
-
-    @objc open func set(playerItem: AVPlayerItem) {
         // Prepare the old item for removal
+        
         if let currentItem = self.player.currentItem {
             self.removePlayerItemObservers(fromPlayerItem: currentItem)
         }
-
+        
         // Replace it with the new item
+        
+        let playerItem = AVPlayerItem(asset: asset)
+        
         self.addPlayerItemObservers(toPlayerItem: playerItem)
+        
         self.player.replaceCurrentItem(with: playerItem)
     }
     
-    // MARK: - ProvidesView
+    // MARK: ProvidesView
     
-    private class RegularPlayerView: PlayerView {
+    private class RegularPlayerView: UIView {
         var playerLayer: AVPlayerLayer {
             return self.layer as! AVPlayerLayer
         }
-
-        #if canImport(UIKit)
+        
         override class var layerClass: AnyClass {
             return AVPlayerLayer.self
         }
-        #elseif canImport(AppKit)
-        override init(frame frameRect: NSRect) {
-            super.init(frame: frameRect)
-            self.layer = AVPlayerLayer()
-        }
-
-        required init?(coder decoder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
-        #endif
         
         func configureForPlayer(player: AVPlayer) {
             (self.layer as! AVPlayerLayer).player = player
         }
     }
     
-    open var view: UIView {
-        return self.regularPlayerView
+    public let view: UIView = RegularPlayerView(frame: .zero)
+    
+    private var regularPlayerView: RegularPlayerView {
+        return self.view as! RegularPlayerView
     }
     
-    // MARK: - Player
+    private var playerLayer: AVPlayerLayer {
+        return self.regularPlayerView.playerLayer
+    }
+    
+    // MARK: Player
     
     weak public var delegate: PlayerDelegate?
     
@@ -119,43 +104,35 @@ extension AVMediaSelectionOption: TextTrackMetadata {
         return self.player.rate > 0
     }
     
-    public var ended: Bool {
-        return self.time >= self.duration
-    }
-    
     public var error: NSError? {
         return self.player.errorForPlayerOrItem
     }
     
-    open func seek(to time: TimeInterval) {
+    public func seek(to time: TimeInterval) {
         let cmTime = CMTimeMakeWithSeconds(time, preferredTimescale: Int32(NSEC_PER_SEC))
-        self.smoothSeek(to: cmTime)
+        
+        self.player.seek(to: cmTime)
+        
+        self.time = time
     }
-
-    open func play() {
+    
+    public func play() {
         self.player.play()
     }
     
-    open func pause() {
+    public func pause() {
         self.player.pause()
     }
     
-    // MARK: - Lifecycle
-
-    override public convenience init() {
-        self.init(seekTolerance: nil)
-    }
+    // MARK: Lifecycle
     
-    public init(seekTolerance: TimeInterval?) {
-        self.regularPlayerView = RegularPlayerView(frame: .zero)
-        self.seekTolerance = seekTolerance.map {
-            CMTimeMakeWithSeconds($0, preferredTimescale: Int32(NSEC_PER_SEC))
-        }
-
+    public override init() {
         super.init()
         
         self.addPlayerObservers()
+        
         self.regularPlayerView.configureForPlayer(player: self.player)
+        
         self.setupAirplay()
     }
     
@@ -167,69 +144,13 @@ extension AVMediaSelectionOption: TextTrackMetadata {
         self.removePlayerObservers()
     }
     
-    // MARK: - Setup
-
-    @available(iOS 10.0, tvOS 10.0, macOS 10.12, *)
-    public var automaticallyWaitsToMinimizeStalling: Bool {
-        get {
-            return self.player.automaticallyWaitsToMinimizeStalling
-        }
-        set {
-            self.player.automaticallyWaitsToMinimizeStalling = newValue
-        }
-    }
+    // MARK: Setup
     
     private func setupAirplay() {
-        #if os(iOS) || os(tvOS)
-            self.player.usesExternalPlaybackWhileExternalScreenIsActive = true
-        #endif
+        self.player.usesExternalPlaybackWhileExternalScreenIsActive = true
     }
-
-    // MARK: - Smooth Seeking
-
-    // Note: Smooth seeking follows the guide from Apple Technical Q&A: https://developer.apple.com/library/archive/qa/qa1820/_index.html
-    // Update the seek target and begin seeking if there is no seek currently in progress.
-    private func smoothSeek(to cmTime: CMTime) {
-        self.seekTarget = cmTime
-
-        guard self.isSeekInProgress == false else { return }
-        self.seekToTarget()
-    }
-
-    // Unconditionally seek to the current seek target.
-    private func seekToTarget() {
-        self.isSeekInProgress = true
-
-        guard self.player.status != .unknown else { return }
-
-        assert(CMTIME_IS_VALID(self.seekTarget))
-        let inProgressSeekTarget = self.seekTarget
-
-        let completion: (Bool) -> Void = { [weak self] _ in
-            guard let self = self else { return }
-
-            self.time = CMTimeGetSeconds(inProgressSeekTarget)
-            if CMTimeCompare(inProgressSeekTarget, self.seekTarget) == 0 {
-                self.isSeekInProgress = false
-            } else {
-                self.seekToTarget()
-            }
-        }
-
-        if let tolerance = self.seekTolerance {
-            self.player.seek(
-                to: inProgressSeekTarget,
-                toleranceBefore: tolerance,
-                toleranceAfter: tolerance,
-                completionHandler: completion
-            )
-        } else {
-            self.player.seek(to: inProgressSeekTarget, completionHandler: completion)
-        }
-    }
-
     
-    // MARK: - Observers
+    // MARK: Observers
     
     private struct KeyPath {
         struct Player {
@@ -317,7 +238,7 @@ extension AVMediaSelectionOption: TextTrackMetadata {
     }
     
     // MARK: Observation Helpers
-
+    
     private func playerItemStatusDidChange(status: AVPlayerItem.Status) {
         switch status {
         case .unknown:
@@ -327,17 +248,8 @@ extension AVMediaSelectionOption: TextTrackMetadata {
         case .readyToPlay:
             
             self.state = .ready
-
-            // If we tried to seek before the video was ready to play, resume seeking now.
-            if self.isSeekInProgress {
-                self.seekToTarget()
-            }
             
         case .failed:
-            
-            self.state = .failed
-        
-        @unknown default:
             
             self.state = .failed
         }
@@ -361,10 +273,9 @@ extension AVMediaSelectionOption: TextTrackMetadata {
         self.bufferedTime = bufferedTime
     }
     
-    // MARK: - Capability Protocol Helpers
+    // MARK: Capability Protocol Helpers
     
     #if os(iOS)
-    @available(iOS 9.0, *)
     fileprivate lazy var _pictureInPictureController: AVPictureInPictureController? = {
         AVPictureInPictureController(playerLayer: self.regularPlayerView.playerLayer)
     }()
